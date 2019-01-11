8b421294c4c041261247d3fad3218545c4738194
金子般的心 <yjx_1wyf@163.com>
2019-01-11 18:24:27 +0800

    update add drew imaging lib/find image create time

--- src/main/java/com/yjx/commonsimaging/ImagingSample.java
+++ src/main/java/com/yjx/commonsimaging/ImagingSample.java
@@ -1,7 +1,17 @@
 package com.yjx.commonsimaging;
 
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
+import com.drew.metadata.file.FileSystemDirectory;
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sanselan.ImageFormat;
+import org.apache.sanselan.ImageInfo;
 import org.apache.sanselan.ImageReadException;
 import org.apache.sanselan.Sanselan;
 import org.apache.sanselan.common.IImageMetadata;
@@ -14,14 +24,22 @@ import javax.imageio.stream.ImageOutputStream;
 import java.awt.*;
 import java.awt.color.ColorSpace;
 import java.awt.geom.Rectangle2D;
-import java.awt.image.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.ComponentColorModel;
+import java.awt.image.DataBuffer;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @Version 1.0
@@ -32,55 +50,222 @@ import java.util.Objects;
  */
 @Slf4j
 public class ImagingSample {
-    private static File picFolder = new File("/Users/junxiaoyang/Documents/testdata/imageio/");
+    private final static File SOURCE_PATH = new File("/Users/junxiaoyang/OneDrive/3寸-无白边-加日期/");
+    private final static File TARGET_PATH = new File("/Users/junxiaoyang/Documents/testdata/imageio/adddate/");
 
     public static void main(String[] args) {
-        log.info(picFolder.getPath());
-        ArrayList<File> isFiles = Lists.newArrayList();
-        if (picFolder.isDirectory()) {
-            isFiles.addAll(Arrays.asList(Objects.requireNonNull(picFolder.listFiles())));
+        ArrayList<File> sourceFiles = Lists.newArrayList();
+        if (SOURCE_PATH.isDirectory()) {
+            sourceFiles.addAll(Arrays.asList(Objects.requireNonNull(SOURCE_PATH.listFiles())));
         }
-        for (File isFile : isFiles) {
-            log.info(isFile.getName());
-            try {
-                writetofile(isFile);
-            } catch (ImageReadException | IOException e) {
-                e.printStackTrace();
+        for (File sourceFile : sourceFiles) {
+            if (!sourceFile.getName().equals("IMG_9359.JPG")) {
+                continue;
             }
+//            revertName(sourceFile);
+//            log.info(sourceFile.getName());
+            String dateFormatted = getDate(sourceFile);
+//            if (isFile.getName().startsWith(NO_METADATA) || isFile.getName().startsWith(DATA_BLOCK) || isFile.getName().startsWith(NO_DATE)) {
+//                continue;
+//            }
+//            if (isFile.getName().startsWith("IMG")) {
+//                try {
+//                    writetofile(isFile);
+//                } catch (Exception e) {
+//                    log.error("file: {} message: {}", isFile.getName(), e.getMessage());
+//                }
+//            }
         }
-
+        log.info("no metadata files {}, block files {}", noMetaDataIndex.get(), blockFilesIndex.get());
     }
 
-    static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    private static void writetofile(File file) throws ImageReadException, IOException {
-        IImageMetadata imageMetadata = Sanselan.getMetadata(file);
-        if (imageMetadata instanceof JpegImageMetadata) {
-            JpegImageMetadata jpegImageMetadata = (JpegImageMetadata) imageMetadata;
-            TiffField createDateField = jpegImageMetadata.findEXIFValue(TiffConstants.EXIF_TAG_CREATE_DATE);
-            log.info("name: {} value: {}", createDateField.getTagName(), createDateField.getValue());
-            File osFile = new File(picFolder + "/datewatermark_" + file.getName());
-            String date = createDateField.getValue().toString();
-            boolean endsWith = date.endsWith("\u0000");
-            if (endsWith) {
-                String subdate = date.substring(0, date.length() - 1);
-                LocalDateTime localDateTime = LocalDateTime.parse(subdate, DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ss"));
-                String format = localDateTime.format(formatter);
-                System.out.println(format);
-                addTextWatermark(file, format, osFile);
+    static AtomicInteger noMetaDataIndex = new AtomicInteger(0);
+    static AtomicInteger blockFilesIndex = new AtomicInteger(0);
+    private static final String NO_METADATA = "NoMetadata_";
+    private static final String DATA_BLOCK = "Datablock_";
+    private static final String NO_DATE = "NoDate_";
+
+    private static void writetofile(File source) {
+        ImageInfo imageInfo;
+        IImageMetadata imageMetadata;
+        try {
+            imageInfo = Sanselan.getImageInfo(source);
+            imageMetadata = Sanselan.getMetadata(source);
+        } catch (ImageReadException | IOException e) {
+            blockFilesIndex.incrementAndGet();
+            rename(source, DATA_BLOCK + source.getName());
+            return;
+        }
+        if (imageInfo != null) {
+            if (!ImageFormat.IMAGE_FORMAT_JPEG.name.equals(imageInfo.getFormat().name)) {
+                log.info("{} image type is {}", source.getName(), imageInfo.getFormatName());
             }
-
+        }
+        if (imageMetadata == null) {
+            log.warn("{} no metadata", source.getName());
+            noMetaDataIndex.incrementAndGet();
+            rename(source, NO_METADATA + source.getName());
+            return;
+            //            moveFile(file.getName());
+        }
+        try {
+            if (imageMetadata instanceof JpegImageMetadata) {
+                JpegImageMetadata jpegImageMetadata = (JpegImageMetadata) imageMetadata;
+                TiffField createDateField = jpegImageMetadata.findEXIFValue(TiffConstants.EXIF_TAG_CREATE_DATE);
+                File osFile = new File(TARGET_PATH.getPath() + "/" + source.getName());
+                String date = createDateField.getValue().toString();
+                boolean endsWith = date.endsWith("\u0000");
+                if (endsWith) {
+                    date = date.substring(0, date.length() - 1);
+                    LocalDateTime localDateTime = LocalDateTime.parse(date, DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ss"));
+                    date = localDateTime.format(FORMATTER);
+                    log.info("name: {} date: {} after: {}", source.getName(), createDateField.getValue(), date);
+                }
+                addTextWatermark(source, date, osFile, imageInfo.getFormat().name);
+            }
+        } catch (ImageReadException | IOException | NullPointerException e) {
+            log.warn("no create date {}", source.getName());
+            rename(source, NO_DATE + source.getName());
         }
     }
 
-    private static void addTextWatermark(File source, String text, File destination) throws IOException {
+
+    private static String getDate(File source) {
+        String dateFormatted;
+        try {
+            Metadata metadata = ImageMetadataReader.readMetadata(source);
+            //1.metadata
+            dateFormatted = getDateTimeFromDirectory(metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class));
+            if (dateFormatted != null) {
+                return dateFormatted;
+            }
+            log.warn("{} no datetime in exif directory use drew imaging", source.getName());
+            //2.file name
+            dateFormatted = getDateTimeFromFileName(source);
+            //3.modifytime
+            dateFormatted = getDateTimeFromDirectory(metadata.getFirstDirectoryOfType(FileSystemDirectory.class));
+            if (dateFormatted != null) {
+                return dateFormatted;
+            }
+            log.warn("{} no datetime in file use drew imaging", source.getName());
+            //            printAllTags(source);
+            log.info("{} create time is {} ", source.getName(), dateFormatted);
+            return null;
+        } catch (ImageProcessingException | IOException e) {
+            log.error("{} get metadata error", source.getName());
+        }
+        return null;
+    }
+
+    private static String getDateTimeFromFileName(File source) {
+        //TODO
+        return null;
+    }
+
+    private static String formatDateTime(Date date) {
+        String dateFormatted;
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+        dateFormatted = localDateTime.format(FORMATTER);
+        return dateFormatted;
+    }
+
+    private static String getDateTimeFromDirectory(ExifSubIFDDirectory directory) {
+        if (directory == null) {
+            return null;
+        }
+        String dateTime = null;
+        Date date = directory.getDateOriginal();
+        if (date != null) {
+            dateTime = formatDateTime(date);
+        }
+        return dateTime;
+    }
+
+    private static String getDateTimeFromDirectory(FileSystemDirectory directory) {
+        String dateTime = null;
+        if (directory == null) {
+            return null;
+        }
+        Date date = directory.getDate(FileSystemDirectory.TAG_FILE_MODIFIED_DATE);
+        if (date != null) {
+            dateTime = formatDateTime(date);
+        }
+        return dateTime;
+    }
+
+    private static void printAllTags(File file) {
+        try {
+            IImageMetadata metadata = Sanselan.getMetadata(file);
+            if (metadata == null) {
+                log.warn("{} not fount metadata use imaging ", file.getName());
+                return;
+            }
+            for (Object item : metadata.getItems()) {
+                log.info(item.toString());
+            }
+        } catch (ImageReadException | IOException e) {
+            log.warn("{} not fount metadata use imaging ", file.getName());
+        }
+    }
+
+    private static void printAllTags(Iterable<Directory> directories) {
+        for (Directory directory : directories) {
+            for (Tag tag : directory.getTags()) {
+                log.info("{}, {}, {}", tag.getDirectoryName(), tag.getTagName(), tag.toString());
+            }
+        }
+    }
+
+    private static String getFormat(File source) {
+        if (StringUtils.endsWithIgnoreCase(source.getName(), "JPG")) {
+            return "JPEG";
+        }
+        if (StringUtils.endsWithIgnoreCase(source.getName(), "PNG")) {
+            return "PNG";
+        }
+        return null;
+    }
+
+    private static void revertName(File file) {
+        String newName = null;
+        if (StringUtils.startsWith(file.getName(), NO_DATE)) {
+            newName = StringUtils.substring(file.getName(), NO_DATE.length());
+        }
+        if (StringUtils.startsWith(file.getName(), NO_METADATA)) {
+            newName = StringUtils.substring(file.getName(), NO_METADATA.length());
+        }
+        if (StringUtils.startsWith(file.getName(), DATA_BLOCK)) {
+            newName = StringUtils.substring(file.getName(), DATA_BLOCK.length());
+        }
+        if (newName == null) {
+            return;
+        }
+        rename(file, newName);
+    }
+
+    private static void rename(File file, String newName) {
+        log.info("rename {} to {}", file.getName(), newName);
+        boolean b = file.renameTo(new File(SOURCE_PATH.getPath() + "/" + newName));
+        if (!b) {
+            log.error("{} rename fail", file.getName());
+        }
+    }
+
+    private static void moveFile(String name) throws IOException {
+        log.info("move file {}", name);
+        Files.move(Paths.get(SOURCE_PATH + name), Paths.get(TARGET_PATH + name));
+    }
+
+    private static void addTextWatermark(File source, String text, File destination, String imageFormat) throws IOException {
         //Get Reader
-        ImageReader jpgReader = getReader(source);
+        ImageReader jpgReader = getReader(source, imageFormat);
         BufferedImage imageSource = jpgReader.read(0);
         //draw image
         BufferedImage bufferedImageOutput = drawText(text, imageSource);
         //get and config writer
-        ImageWriter jpegWriter = ImageIO.getImageWritersByFormatName("jpeg").next();
+        ImageWriter jpegWriter = getImageWriter(imageFormat);
         ImageWriteParam writeParam = jpegWriter.getDefaultWriteParam();
         writeParam.setProgressiveMode(ImageWriteParam.MODE_COPY_FROM_METADATA);
         writeParam.setCompressionMode(ImageWriteParam.MODE_COPY_FROM_METADATA);
@@ -93,8 +278,12 @@ public class ImagingSample {
         }
     }
 
-    private static ImageReader getReader(File file) throws IOException {
-        ImageReader jpgReader = ImageIO.getImageReadersByFormatName("jpg").next();
+    private static ImageWriter getImageWriter(String imageFormat) {
+        return ImageIO.getImageWritersByFormatName(imageFormat).next();
+    }
+
+    private static ImageReader getReader(File file, String imageFormat) throws IOException {
+        ImageReader jpgReader = ImageIO.getImageReadersByFormatName(imageFormat).next();
         jpgReader.setInput(ImageIO.createImageInputStream(file));
         return jpgReader;
     }
@@ -109,23 +298,28 @@ public class ImagingSample {
 
         Graphics2D textGraphics = (Graphics2D) watermarked.getGraphics();
         textGraphics.drawImage(imageSource, 0, 0, null);
-        AlphaComposite alphaChannel = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f);
+        AlphaComposite alphaChannel = AlphaComposite.getInstance(AlphaComposite.SRC_OVER);
         textGraphics.setComposite(alphaChannel);
         //微软雅黑 粗体 26号字  投影4像素，或是描边4像素
         textGraphics.setColor(Color.BLACK);
-        textGraphics.setFont(new Font("TypoSlab Irregular Shadowed Demo", Font.BOLD, 100));
+        textGraphics.setFont(getFont("TypoSlab Irregular Shadowed Demo"));
 //        textGraphics.setFont(new Font("Here & Not Found", Font.BOLD, 100));
         FontMetrics fontMetrics = textGraphics.getFontMetrics();
         Rectangle2D rect = fontMetrics.getStringBounds(text, textGraphics);
 
         // calculate center of the imageSource
-        int centerX = imageSource.getWidth() - ((int) rect.getWidth() + 100);
-        int centerY = imageSource.getHeight() - ((int) rect.getHeight() + 50);
+        int centerX = imageSource.getWidth() - ((int) rect.getWidth() + 20);
+        int centerY = imageSource.getHeight() - ((int) rect.getHeight() + 5);
 
 //        setGrayRGB(imageSource, watermarked);
         // add text overlay to the imageSource
         textGraphics.drawString(text, centerX, centerY);
         return watermarked;
+    }
+
+    private static Font getFont(String text) {
+        Font font = new Font(text, Font.BOLD, 100);
+        return font;
     }
 
     private static void setGrayRGB(BufferedImage imageSource, BufferedImage watermarked) {
